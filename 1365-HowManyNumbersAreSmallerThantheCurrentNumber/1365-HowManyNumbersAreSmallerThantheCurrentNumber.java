a6dbba1e5d2fb33be24d26e21d5ef866d7e95768
Onur Haktan <onurhaktan243@gmail.com>
2024-07-24 23:50:21 +0300

    Create file

---
+++ 1365-HowManyNumbersAreSmallerThantheCurrentNumber/1365-HowManyNumbersAreSmallerThantheCurrentNumber.java
@@ -0,0 +1,25 @@
+            int count = 0;
+            for(int j=0; j<nums.length; j++){
+                if(nums[i] > nums[j]){
+            }
+        }
+                    count+=1;
+                }
+            result[i] = count;
+        return result;
+        
+        for(int i=0; i<nums.length; i++){
+        int result[] = new int[nums.length];
+        
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+class Solution {
+[
+[8,1,2,2,3]
+[6,5,4,8]
+[7,7,7,7]
+[4,0,1,1,3]
+[2,1,0,3]
+[0,0,0,0]
+[4,0,1,1,3]
+[2,1,0,3]
+[0,0,0,0]
